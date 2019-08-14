86b426949b7f7a7dd9bf4e11ad2ec770c1bb80ae
groupme@gmail.com <groupme@gmail.com>
2019-08-14 17:49:23 +0200

    update

--- src/activitysim/activitysim.py
+++ src/activitysim/activitysim.py
@@ -12,6 +12,13 @@ from scipy.stats import gaussian_kde
 from pyqt_fit import kde, kde_methods
 from sklearn.neighbors import KernelDensity
 
+
+import json
+
+from pandas.compat import range, lrange, lmap, zip
+from pandas import Series
+from pandas.plotting import autocorrelation_plot
+
 class SQLSource(object):
 
     """A class for iterating the result set of a single SQL query."""
@@ -373,7 +380,42 @@ def run_length_encode(l):
     return [(name, len(list(group))) for name, group in groupby(l)]
 
 
+def auto_corr(mydata):
+    n = len(mydata)
+    lags = n
+    data = np.asarray(mydata)
+    ax = plt.gca(xlim=(1, lags), ylim=(-1.0, 1.0))
+    mean = np.mean(data)
+    c0 = np.sum((data - mean) ** 2) / float(n)
 
+
+    def r(h):
+        return ((data[:n - h] - mean) *
+                (data[h:] - mean)).sum() / float(n) / c0
+
+    x = np.arange(lags) + 1
+    y = lmap(r, x)
+    z95 = 1.959963984540054
+    z99 = 2.5758293035489004
+    ax.axhline(y=z99 / np.sqrt(n), linestyle='--', color='grey')
+    ax.axhline(y=z95 / np.sqrt(n), color='grey')
+    ax.axhline(y=0.0, color='black')
+    ax.axhline(y=-z95 / np.sqrt(n), color='grey')
+    ax.axhline(y=-z99 / np.sqrt(n), linestyle='--', color='grey')
+    ax.set_xlabel("Lag")
+    ax.set_ylabel("Autocorrelation")
+    ax.plot(x, y)
+    ax.grid()
+    plt.show()
+    
+def calculate_autocorr():
+    f = open('/tmp/real.csv', 'r')
+    #f = open('/tmp/syn.csv', 'r')
+    for line in f.readlines():
+        line = line.rstrip('}\n').lstrip('{')
+        values = [float(d) for d in line.split(',')]
+        autocorrelation_plot(values[:130])
+    plt.show()   
 
 
 
